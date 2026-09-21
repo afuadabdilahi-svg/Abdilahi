e83cdd53c99432fdb653d0d344466b5a0bc65cb6
afuadabdilahi-svg <afuadabdilahi@gmail.com>
2026-09-21 17:54:17 +0200

    Clean up comments in tema03 eksempel 1.php
    Removed commented-out code and explanations for clarity.

--- tema03 eksempel 1.php
+++ tema03 eksempel 1.php
@@ -10,22 +10,10 @@
     Trykk Fortsett for &aring; skrive ut tallene fra 1 til 10  <br/>
     <input type="submit" value="Fortsett" id="fortsett" name="fortsett" /> <br />
     <?php   
-/*
-/*    Programmet skriver ut tallene fra 1 til 10 på hver sin linje
-/*    Eksempelet viser bruk av for-setning
-*/
   for ($tall=1;$tall<=10;$tall++)  
     {
       print("$tall <br/>");  
     }
-
-/*
-startverdi: $tall=1 
-betingelse: $tall<=10
-økning: $tall++ (dette er det samme som $tall=$tall+1)
-*/
-
-?>
   </form>
 </body>
 </html>
